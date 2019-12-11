8aab347ecda359726248b626f4ecfe2ba7f17165
Jendrik A. Potyka <jendrik@potyka.net>
2019-12-11 18:53:01 +0100

    Update first functions.

--- RPG-Portals/src/main/java/net/potyka/jendrik/rpgp/App.java
+++ RPG-Portals/src/main/java/net/potyka/jendrik/rpgp/App.java
@@ -1,17 +1,19 @@
 package net.potyka.jendrik.rpgp;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Hello world!
  */
-public final class App {
+public final class App extends JavaPlugin{
     private App() {
     }
-
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
     }
 }
