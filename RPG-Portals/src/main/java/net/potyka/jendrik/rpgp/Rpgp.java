62c56cca234906445aa7315c7611beeab142b513
Jendrik A. Potyka <jendrik@potyka.net>
2019-12-18 00:25:33 +0100

    added towny buttons

--- RPG-Portals/src/main/java/net/potyka/jendrik/rpgp/Rpgp.java
+++ RPG-Portals/src/main/java/net/potyka/jendrik/rpgp/Rpgp.java
@@ -17,9 +17,10 @@ import org.bukkit.ChatColor;
 
 public class Rpgp implements CommandExecutor
 {
+    private App app;
     public Rpgp(App app)
     {
-
+        this.app = app;
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
@@ -50,36 +51,64 @@ public class Rpgp implements CommandExecutor
     
     public void openGUI(Player player)
     {
-        Inventory inv = Bukkit.createInventory(null, 36, ChatColor.DARK_AQUA+"RPG-Portals: Main menu");
+        if(player.hasPermission("rpgp.opengui"))
+        {
+            Inventory inv = Bukkit.createInventory(null, 36, ChatColor.DARK_AQUA+"RPG-Portals: Main menu");
 
-        // button to close the GUI
-        ItemStack exititem = new ItemStack(Material.REDSTONE_BLOCK);
-        ItemMeta meta = exititem.getItemMeta();
-        meta.setDisplayName(ChatColor.RED + "Exit");
-        exititem.setItemMeta(meta);
-        inv.setItem(31, exititem);
+            // button to close the GUI
+            ItemStack exititem = new ItemStack(Material.REDSTONE_BLOCK);
+            ItemMeta meta = exititem.getItemMeta();
+            meta.setDisplayName(ChatColor.RED + "Exit");
+            exititem.setItemMeta(meta);
+            inv.setItem(31, exititem);
 
-        // button for portal to own home
-        ItemStack homeportal = new ItemStack(Material.RED_BED);
-        meta =  homeportal.getItemMeta();
-        meta.setDisplayName(ChatColor.AQUA + "Portal to your Home.");
-        ArrayList<String> homeportal_lore = new ArrayList<>();
-        homeportal_lore.add(ChatColor.DARK_AQUA + "You need:");
-        meta.setLore(homeportal_lore);
-        homeportal.setItemMeta(meta);
-        inv.setItem(0, homeportal);
+            // button for portal to own home
+            ItemStack homeportal = new ItemStack(Material.RED_BED);
+            meta =  homeportal.getItemMeta();
+            meta.setDisplayName(ChatColor.AQUA + "Portal to your Home.");
+            ArrayList<String> homeportal_lore = new ArrayList<>();
+            homeportal_lore.add(ChatColor.DARK_AQUA + "You need:");
+            meta.setLore(homeportal_lore);
+            homeportal.setItemMeta(meta);
+            inv.setItem(0, homeportal);
 
-        // butto for portal to the end
-        ItemStack endportal = new ItemStack(Material.ENDER_EYE);
-        meta =  endportal.getItemMeta();
-        meta.setDisplayName(ChatColor.AQUA + "Portal to the end.");
-        ArrayList<String> endportal_lore = new ArrayList<>();
-        endportal_lore.add(ChatColor.DARK_AQUA + "You need:");
-        meta.setLore(endportal_lore);
-        endportal.setItemMeta(meta);
-        inv.setItem(1, endportal);
+            // button for portal to the end
+            ItemStack endportal = new ItemStack(Material.ENDER_EYE);
+            meta =  endportal.getItemMeta();
+            meta.setDisplayName(ChatColor.AQUA + "Portal to the end.");
+            ArrayList<String> endportal_lore = new ArrayList<>();
+            endportal_lore.add(ChatColor.DARK_AQUA + "You need:");
+            meta.setLore(endportal_lore);
+            endportal.setItemMeta(meta);
+            inv.setItem(1, endportal);
 
-        player.openInventory(inv);
+            if(app.getUseTonwy())
+            {
+                // button for portal to player town
+                ItemStack hometownportal = new ItemStack(Material.BLUE_BANNER);
+                meta =  hometownportal.getItemMeta();
+                meta.setDisplayName(ChatColor.AQUA + "Portal to your home town.");
+                ArrayList<String> hometownportal_lore = new ArrayList<>();
+                hometownportal_lore.add(ChatColor.DARK_AQUA + "You need:");
+                meta.setLore(hometownportal_lore);
+                hometownportal.setItemMeta(meta);
+                inv.setItem(2, hometownportal);
+                player.openInventory(inv); 
+
+                // button for other town list
+                ItemStack othertownportal = new ItemStack(Material.RED_BANNER);
+                meta =  othertownportal.getItemMeta();
+                meta.setDisplayName(ChatColor.AQUA + "List of other public towns.");
+                ArrayList<String> othertownportal_lore = new ArrayList<>();
+                othertownportal_lore.add(ChatColor.DARK_AQUA + "Click to open it.");
+                meta.setLore(othertownportal_lore);
+                othertownportal.setItemMeta(meta);
+                inv.setItem(2+9, othertownportal);
+            }           
+
+            player.openInventory(inv);  
+        }
+
 
 
 
